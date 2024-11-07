a2b13a4ee2732deb8941c542f80000f41c5680c1
Luis Hernández <joseluis120802.2@gmail.com>
2024-11-06 19:03:24 -0600

    Agregando funcion para obtener el sesgo en valores numericos

--- analisisUnivariable.py
+++ analisisUnivariable.py
@@ -4,11 +4,12 @@ dataFrame = pd.read_csv('./student_sleep_patterns.csv')
 print(dataFrame)
 
 print("Analisis estadistico")
-for column in dataFrame:
-    print("Nombre de atributo: ",column)
-    print("Tipo de atributo: ",dataFrame[column].dtype)
+for column in dataFrame.iloc[:, 1:]:
+    print(f'Nombre de atributo: {column}')
+    print(f'Tipo de atributo: {dataFrame[column].dtype}')
     print(dataFrame[column].describe())
     print()
+   
     fig = plt.figure()
     if dataFrame[column].dtype == object:
         fig.canvas.manager.set_window_title("Histograma de columna: "+str(column))
@@ -17,6 +18,8 @@ for column in dataFrame:
         plt.title('Columna: '+column)
         plt.show()
     else:
+        sesgo = dataFrame[column].skew()
+        print(f'Sesgo de la columna {column}: {sesgo}')
         fig.canvas.manager.set_window_title("Grafica de caja de columna: "+str(column))
         dataFrame[column].plot(kind='box',figsize=(10,10))
         plt.title('Columna: '+column)
