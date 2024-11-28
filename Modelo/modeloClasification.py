c562013799bc6003f14314346275642abf66f001
Luis Hernández <joseluis120802.2@gmail.com>
2024-11-27 18:53:38 -0600

    Agregando resultados de recall por columna en modelo de clasificacion

--- Modelo/modeloClasification.py
+++ Modelo/modeloClasification.py
@@ -4,6 +4,7 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.model_selection import KFold
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import accuracy_score
+from sklearn.metrics import recall_score
 import numpy as np
 dataFrame = pd.read_csv('./DatasetClasification.csv')
 
@@ -14,6 +15,8 @@ matrizLRArray=[]
 matrizGNBArray=[]
 exactitudGNB=[]
 exactitudLRA=[]
+recallGNBArray=[]
+recallLRAArray=[]
 for i, (train_index, test_index) in enumerate(kf.split(dataFrame)):
     X_train=dataFrame.iloc[train_index].drop(columns=['Sleep_Quality'])
     y_train=dataFrame.iloc[train_index]['Sleep_Quality']
@@ -31,6 +34,12 @@ for i, (train_index, test_index) in enumerate(kf.split(dataFrame)):
     exactitudGNB.append(precision)
     precision=(accuracy_score(y_test,resultLog))
     exactitudLRA.append(precision)
+
+    recallLOG=(recall_score(y_test,resultLog,average=None,zero_division=np.nan))
+    recallGNB=(recall_score(y_test,resultGNB,average=None,zero_division=np.nan))
+    recallLRAArray.append(recallLOG)
+    recallGNBArray.append(recallGNB)
+    
     
 print("Matriz de confusion promedio de logistic Regression")
 promedio = np.mean(matrizLRArray,axis=0)
@@ -38,9 +47,16 @@ print(promedio)
 print("Promedio de exactitud de logistic Regression")
 promedio = np.mean(exactitudLRA)
 print(promedio*100)
+print("Promedio de recall por clase en logistic Regression")
+promedio = np.mean(recallLRAArray,axis=0)
+print(promedio)
+print('-'*80)
 print("Matriz de confusion promedio de naive bayes")
 promedio = np.mean(matrizGNBArray,axis=0)
 print(promedio)
 print("Promedio de exactitud de naive bayes")
 promedio = np.mean(exactitudGNB)
 print(promedio*100)
+print("Promedio de recall por clase en naive bayes")
+promedio = np.mean(recallGNBArray,axis=0)
+print(promedio)
